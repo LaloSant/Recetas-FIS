5ac626035ab77f3247b828cc2fcb54eaac51afbc
yael8895 <yael.sampayo.01082005@gmail.com>
2026-03-16 16:32:17 -0600

    quick fix

--- src/main/java/com/flerchante/recetario/controller/UsuarioController.java
+++ src/main/java/com/flerchante/recetario/controller/UsuarioController.java
@@ -5,19 +5,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.flerchante.recetario.service.usuario.UserUtils;
 import com.flerchante.recetario.service.usuario.UsuarioServiceImpl;
+
 
 @Controller
 public class UsuarioController {
 
-	private static final String RUTA_VISTA = "/vistas/usuarios/";
-
+	private final UserUtils userUtils;
+ 
+	
 	@Autowired
 	private UsuarioServiceImpl repositorioUsuario;
+
+	private static final String RUTA_VISTA = "/vistas/usuarios/";
+
+	public UsuarioController(UserUtils userUtils) {
+        this.userUtils = userUtils;
+    }
 
 	@GetMapping("/usuarios")
 	public String listarUsuarios(Model modelo) {
 		modelo.addAttribute("usuarios", repositorioUsuario.listarTodoUsuario());
 		return RUTA_VISTA + "usuarios";
 	}
+
+	@GetMapping("/editar")
+	public String editarUsuario(Model modelo) {
+		return RUTA_VISTA+ "editar";
+	}
+	
 }
